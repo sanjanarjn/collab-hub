f15ff0152eb9d27a51d7329bc484f09e002c61a7
Sanjana Rajan <sanjana@Sanjanas-MBP.attlocal.net>
2023-10-18 11:27:57 -0500

    Changing the cache storage model

--- src/main/java/com/nasa/collab/hub/cache/RedisCacheService.java
+++ src/main/java/com/nasa/collab/hub/cache/RedisCacheService.java
@@ -9,14 +9,21 @@ import com.nasa.collab.hub.github.sdk.GithubRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import redis.clients.jedis.JedisPooled;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class RedisCacheService implements CacheService {
+
+    public static final String REPOS_CACHE_KEY_SUFFIX = "_repos";
+    private static final String ISSUES_CACHE_KEY_SUFFIX = "_issues";
 
     private RedisConnectionConfiguration redisConnectionConfiguration;
     private JedisPooled jedisPool;
@@ -32,7 +39,12 @@ public class RedisCacheService implements CacheService {
     @Override
     public void putRepositoriesForUserInCache(long userId, List<GithubRepository> repositories) {
         try {
-            this.jedisPool.setex(userId + "_repos", redisConnectionConfiguration.getDefaultTTLInSeconds(), objectMapper.writeValueAsString(repositories));
+            for (GithubRepository githubRepository : repositories) {
+                String repoId = githubRepository.getId();
+                this.jedisPool.lpush(userId + REPOS_CACHE_KEY_SUFFIX, repoId);
+                this.jedisPool.expire(userId + REPOS_CACHE_KEY_SUFFIX, redisConnectionConfiguration.getDefaultTTLInSeconds());
+                this.jedisPool.setex(repoId, redisConnectionConfiguration.getDefaultTTLInSeconds(), objectMapper.writeValueAsString(githubRepository));
+            }
         } catch (JsonProcessingException e) {
             log.warn("Error occured while serializing data before caching: {}", e.getMessage());
         }
@@ -40,22 +52,31 @@ public class RedisCacheService implements CacheService {
 
     @Override
     public Optional<List<GithubRepository>> getRepositoriesForUserFromCache(long userId) {
-        Optional<List<GithubRepository>> repositories = Optional.empty();
-        String repositoriesCached = this.jedisPool.get(userId + "_repos");
+        List<String> repoIdsCached = this.jedisPool.lrange(userId + REPOS_CACHE_KEY_SUFFIX, 0, -1);
+        if (CollectionUtils.isEmpty(repoIdsCached))
+            return Optional.empty();
+
+        List<GithubRepository> repositories = new ArrayList<>();
         try {
-            repositories = repositoriesCached != null ? Optional.of(objectMapper.readValue(repositoriesCached, new TypeReference<List<GithubRepository>>() {
-            })) : repositories;
+            for (String repoId : repoIdsCached) {
+                repositories.add(objectMapper.readValue(this.jedisPool.get(repoId), GithubRepository.class));
+            }
         } catch (JsonProcessingException e) {
             log.warn("Error occured while deserializing data from cache: {}", e.getMessage());
         }
-        return repositories;
+        return Optional.of(repositories);
     }
 
 
     @Override
     public void putIssuesForUserInCache(long userId, List<GithubIssue> issues) {
         try {
-            this.jedisPool.setex(userId + "_issues", redisConnectionConfiguration.getDefaultTTLInSeconds(), objectMapper.writeValueAsString(issues));
+            for (GithubIssue githubIssue : issues) {
+                String issueId = githubIssue.getId();
+                this.jedisPool.lpush(userId + ISSUES_CACHE_KEY_SUFFIX, issueId);
+                this.jedisPool.expire(userId + ISSUES_CACHE_KEY_SUFFIX, redisConnectionConfiguration.getDefaultTTLInSeconds());
+                this.jedisPool.setex(issueId, redisConnectionConfiguration.getDefaultTTLInSeconds(), objectMapper.writeValueAsString(githubIssue));
+            }
         } catch (JsonProcessingException e) {
             log.warn("Error occured while serializing data before caching: {}", e.getMessage());
         }
@@ -63,14 +84,18 @@ public class RedisCacheService implements CacheService {
 
     @Override
     public Optional<List<GithubIssue>> getIssuesForUserFromCache(long userId) {
-        Optional<List<GithubIssue>> issues = Optional.empty();
-        String repositoriesCached = this.jedisPool.get(userId + "_issues");
+        List<String> issueIdsCached = this.jedisPool.lrange(userId + ISSUES_CACHE_KEY_SUFFIX, 0, -1);
+        if (CollectionUtils.isEmpty(issueIdsCached))
+            return Optional.empty();
+
+        List<GithubIssue> issues = new ArrayList<>();
         try {
-            issues = repositoriesCached != null ? Optional.of(objectMapper.readValue(repositoriesCached, new TypeReference<List<GithubIssue>>() {
-            })) : issues;
+            for (String issueId : issueIdsCached) {
+                issues.add(objectMapper.readValue(this.jedisPool.get(issueId), GithubIssue.class));
+            }
         } catch (JsonProcessingException e) {
             log.warn("Error occured while deserializing data from cache: {}", e.getMessage());
         }
-        return issues;
+        return Optional.of(issues);
     }
 }
